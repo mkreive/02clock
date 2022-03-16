6a8a7b18e1d1766efb3482eac56bf1c8680d93fc
mkreive <mkreive@gmail.com>
2022-03-16 12:59:12 +0200

    styling fixed

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Selecting elements
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const location = document.getElementById("location");
+
+// Code
