0cdbd2195e54551cefec9b85411481e5b870c22f
mkreive <mkreive@gmail.com>
2022-03-16 13:23:24 +0200

    variables

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 "use strict";
 
 // Selecting elements
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
-const location = document.getElementById("location");
+const yearElement = document.getElementById("year");
+const monthElement = document.getElementById("month");
+const dayElement = document.getElementById("day");
+const weekdayElement = document.getElementById("weekday");
+
+const hoursElement = document.getElementById("hours");
+const minutesElement = document.getElementById("minutes");
+const secondsElement = document.getElementById("seconds");
+
+const place = document.getElementById("place");
+
+// Variables
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const day = now.getDate();
+const weekday = now.getDay();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
+// Helper functions
+
+const twoDigitFormatter = function (field) {
+    return field.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    });
+};
+
+console.log(now);
+console.log(day);
 
 // Code
+
+yearElement.innerText = year;
+monthElement.innerText = twoDigitFormatter(month);
+dayElement.innerText = twoDigitFormatter(day);
