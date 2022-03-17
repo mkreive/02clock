a63888acd53c5b1a3e0acf217e4436d0097fa25f
mkreive <mkreive@gmail.com>
2022-03-17 10:41:38 +0200

    finished logic

--- script.js
+++ script.js
@@ -9,23 +9,7 @@ const weekdayElement = document.getElementById("weekday");
 const hoursElement = document.getElementById("hours");
 const minutesElement = document.getElementById("minutes");
 const secondsElement = document.getElementById("seconds");
-
 const place = document.getElementById("place");
-
-// Variables
-const now = new Date();
-const year = now.getFullYear();
-const month = now.getMonth() + 1;
-const day = now.getDate();
-
-const hours = now.getHours();
-const minutes = now.getMinutes();
-const seconds = now.getSeconds();
-
-const options = { weekday: "long" };
-const weekday = new Intl.DateTimeFormat("en-US", options).format(now);
-
-// Helper functions
 
 const twoDigitFormatter = function (field) {
     return field.toLocaleString("en-US", {
@@ -34,33 +18,34 @@ const twoDigitFormatter = function (field) {
     });
 };
 
-console.log(now);
+const showTime = function () {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const options = { weekday: "long" };
+    const weekday = new Intl.DateTimeFormat("en-US", options).format(now);
 
-// Time and Date
+    yearElement.innerText = year;
+    monthElement.innerText = twoDigitFormatter(month);
+    dayElement.innerText = twoDigitFormatter(day);
+    weekdayElement.innerText = weekday;
+    hoursElement.innerText = twoDigitFormatter(hours);
+    minutesElement.innerText = twoDigitFormatter(minutes);
+    secondsElement.innerText = twoDigitFormatter(seconds);
 
-yearElement.innerText = year;
-monthElement.innerText = twoDigitFormatter(month);
-dayElement.innerText = twoDigitFormatter(day);
-weekdayElement.innerText = weekday;
-hoursElement.innerText = twoDigitFormatter(hours);
-minutesElement.innerText = twoDigitFormatter(minutes);
-secondsElement.innerText = twoDigitFormatter(seconds);
+    setInterval(showTime, 1000);
+};
 
-// Location
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        place.innerText = "Geolocation is not supported by this browser.";
-    }
-}
+showTime();
 
 function showPosition(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(latitude, longitude);
-    displayLocation(latitude, longitude);
+    place.innerText = `Longitude: ${longitude}, Latitude: ${latitude}`;
 }
 
 function showError(error) {
@@ -80,29 +65,11 @@ function showError(error) {
     }
 }
 
-function displayLocation(latitude, longitude) {
-    let geocoder;
-    geocoder = new google.maps.Geocoder();
-    const latlng = new google.maps.LatLng(latitude, longitude);
-
-    geocoder.geocode({ latLng: latlng }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            if (results[0]) {
-                const add = results[0].formatted_address;
-                const value = add.split(",");
-
-                count = value.length;
-                country = value[count - 1];
-                state = value[count - 2];
-                city = value[count - 3];
-                place.innerText = "city name is: " + city;
-            } else {
-                place.innerText = "address not found";
-            }
-        } else {
-            place.innerText = "Geocoder failed due to: " + status;
-        }
-    });
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        place.innerText = "Geolocation is not supported by this browser.";
+    }
 }
-
-displayLocation();
+getLocation();
